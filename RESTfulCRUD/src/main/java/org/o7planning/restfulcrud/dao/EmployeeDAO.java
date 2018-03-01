be5b82ffd3046a557af4fd1f37da46eb1c58964d
Prashanth Gadapa <prashanth.gadapa@gmail.com>
2018-03-01 12:49:56 +0530

    RESTful Employees example

---
+++ RESTfulCRUD/src/main/java/org/o7planning/restfulcrud/dao/EmployeeDAO.java
@@ -0,0 +1,58 @@
+package org.o7planning.restfulcrud.dao;
+ 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+ 
+import org.o7planning.restfulcrud.model.Employee;
+ 
+public class EmployeeDAO {
+ 
+    private static final Map<String, Employee> empMap = new HashMap<String, Employee>();
+ 
+    static {
+        initEmps();
+    }
+ 
+    private static void initEmps() {
+        Employee emp1 = new Employee("U6065654", "Prashanth Kumar", "TechIntern","Hyderabad");
+        Employee emp2 = new Employee("U6065653", "Mounika Thota", "TechIntern","Hyderabad");
+        Employee emp3 = new Employee("U6065658", "A Raghunandan", "TechIntern","Hyderabad");
+        Employee emp4 = new Employee("U6065660", "Subhadra devi", "TechIntern","Hyderabad");
+ 
+        empMap.put(emp1.getEmpNo(), emp1);
+        empMap.put(emp2.getEmpNo(), emp2);
+        empMap.put(emp3.getEmpNo(), emp3);
+        empMap.put(emp4.getEmpNo(), emp4);
+    }
+ 
+    public static Employee getEmployee(String empNo) {
+        return empMap.get(empNo);
+    }
+ 
+    public static Employee addEmployee(Employee emp) {
+        empMap.put(emp.getEmpNo(), emp);
+        return emp;
+    }
+ 
+    public static Employee updateEmployee(Employee emp) {
+        empMap.put(emp.getEmpNo(), emp);
+        return emp;
+    }
+ 
+    public static void deleteEmployee(String empNo) {
+        empMap.remove(empNo);
+    }
+ 
+    public static List<Employee> getAllEmployees() {
+        Collection<Employee> c = empMap.values();
+        List<Employee> list = new ArrayList<Employee>();
+        list.addAll(c);
+        return list;
+    }
+     
+    List<Employee> list;
+ 
+}
